8fe3837dc84a564945e1c452ff9e90e08eb33131
sum-in-e <suminkim.me@gmail.com>
2025-11-22 22:58:08 +0900

    feat: 메인 서비스 섹션 반응형

--- components/home/ServiceCard.tsx
+++ components/home/ServiceCard.tsx
@@ -76,23 +76,23 @@ export function ServiceCard({
         <Image src={imageSrc} alt={title} fill className="object-cover" />
         {id === "b2b" && <div className="absolute inset-0 bg-[#003BB1B2]" />}
         <div
-          className={`absolute inset-0 flex flex-col justify-between bg-gradient-to-b from-transparent via-transparent ${gradientColor} p-6 sm:p-6 md:p-12 lg:p-14 xl:p-15 2xl:p-16`}
+          className={`absolute inset-0 flex flex-col justify-between bg-gradient-to-b from-transparent via-transparent ${gradientColor} xl:p-15 p-6 sm:p-10 md:p-12 lg:p-14 2xl:p-16`}
         >
           <div>
-            <h3 className="mb-4 text-[32px] font-black leading-[40px] text-white sm:mb-6 sm:text-[40px] sm:leading-[48px] md:mb-10 md:text-[56px] md:leading-[70px] lg:text-[65px] lg:leading-[72px] xl:text-[72px] xl:leading-[78px] 2xl:text-[80px] 2xl:leading-[70px]">
+            <h3 className="mb-4 text-[32px] font-black leading-[40px] text-white sm:mb-[25px] sm:text-[50px] sm:leading-[62px] md:mb-[20px] md:text-[45px] md:leading-[60px] lg:text-[55px] lg:leading-[65px] xl:mb-[28px] xl:text-[65px] xl:leading-[72px] 2xl:mb-[43px] 2xl:text-[80px] 2xl:leading-[70px]">
               {title}
             </h3>
-            <p className="text-base font-medium leading-[18px] text-white sm:text-xl sm:leading-[22px] md:text-[24px] md:leading-[32px] lg:text-[28px] lg:leading-[40px] xl:text-[30px] xl:leading-[50px] 2xl:text-[32px] 2xl:leading-[70px]">
+            <p className="text-base font-medium leading-[18px] text-white sm:text-[24px] sm:leading-[30px] md:text-[18px] md:leading-[26px] lg:text-[20px] lg:leading-[32px] xl:text-[22px] xl:leading-[35px] 2xl:text-[32px] 2xl:leading-[70px]">
               {description}
             </p>
           </div>
           <div>
             <span
-              className={`inline-block rounded-full ${tagBgColor} mb-2 px-3 py-1 text-xs font-bold sm:mb-[20px] sm:px-4 sm:py-2 sm:text-sm md:mb-[30px] md:px-5 md:py-2 md:text-base lg:mb-6 lg:px-5 lg:py-2 lg:text-lg xl:mb-8 xl:px-6 xl:py-3 xl:text-xl 2xl:mb-[30px] 2xl:px-5 2xl:py-2 2xl:text-[32px] 2xl:leading-[30px] ${tagTextColor}`}
+              className={`inline-block rounded-full ${tagBgColor} mb-2 px-3 py-0.5 text-xs font-bold sm:mb-5 sm:px-4 sm:text-base md:mb-4 md:px-5 md:py-1 md:text-base lg:text-lg xl:px-5 xl:py-1 xl:text-xl 2xl:mb-[30px] 2xl:text-[32px] 2xl:leading-[30px] ${tagTextColor}`}
             >
               {tagText}
             </span>
-            <p className="text-sm font-medium text-white sm:text-base md:text-[24px] md:leading-[32px] lg:text-[32px] lg:leading-[40px] xl:text-[36px] xl:leading-[48px] 2xl:text-[40px] 2xl:leading-[40px]">
+            <p className="text-sm font-medium text-white sm:text-[20px] md:text-[22px] md:leading-[30px] lg:text-[22px] lg:leading-[32px] xl:text-[26px] xl:leading-[40px] 2xl:text-[40px] 2xl:leading-[40px]">
               {callToAction}
             </p>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,43 @@
-const {theme} = require('@sanity/demo/tailwind')
+const { theme } = require("@sanity/demo/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     ...theme,
     fontFamily: {
       sans: "'Pretendard Variable', -apple-system, BlinkMacSystemFont, system-ui, 'Segoe UI', 'Helvetica Neue', Arial, sans-serif",
-      serif: 'var(--font-serif)',
-      mono: 'var(--font-mono)',
-      inter: 'var(--font-inter)',
-      'inria-serif': 'var(--font-inria-serif)',
+      serif: "var(--font-serif)",
+      mono: "var(--font-mono)",
+      inter: "var(--font-inter)",
+      "inria-serif": "var(--font-inria-serif)",
     },
     extend: {
       colors: {
-        navy: '#0E1C62',
-        deepnavy: '#0C1D77'
+        navy: "#0E1C62",
+        deepnavy: "#0C1D77",
       },
       maxWidth: {
-        'screen-max': '1920px',
+        "screen-max": "1920px",
       },
       animation: {
-        'shimmer-text-horizontal': 'shimmer-text-horizontal 5s cubic-bezier(0.4, 0, 0.2, 1) infinite',
+        "shimmer-text-horizontal":
+          "shimmer-text-horizontal 5s cubic-bezier(0.4, 0, 0.2, 1) infinite",
       },
       keyframes: {
-        'shimmer-text-horizontal': {
-          '0%': {
-            backgroundPosition: '-100% 0%',
+        "shimmer-text-horizontal": {
+          "0%": {
+            backgroundPosition: "-100% 0%",
           },
-          '20%': {
-            backgroundPosition: '100% 0%',
+          "20%": {
+            backgroundPosition: "100% 0%",
           },
-          '100%': {
-            backgroundPosition: '100% 0%',
+          "100%": {
+            backgroundPosition: "100% 0%",
           },
         },
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
