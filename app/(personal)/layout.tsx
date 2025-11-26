7648d066b32124ae5bb529a7c4644c0a1507a78a
sum-in-e <suminkim.me@gmail.com>
2025-11-26 11:47:54 +0900

    fix: 대시보드 스크롤 안 되는 이슈 해결

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -4,6 +4,8 @@ import {Navbar} from '@/components/Navbar'
 import {CustomCursor} from '@/components/CustomCursor'
 import {IntroAnimation} from '@/components/IntroAnimation'
 import {ScrollToTopButton} from '@/components/ScrollToTopButton'
+import {GSAPScroll} from '@/components/GSAPScroll'
+import {OverlayScrollbarsWrapper} from '@/components/OverlayScrollbarsWrapper'
 import {sanityFetch, SanityLive} from '@/sanity/lib/live'
 import {homePageQuery} from '@/sanity/lib/queries'
 import {urlForOpenGraphImage} from '@/sanity/lib/utils'
@@ -40,20 +42,22 @@ export const viewport: Viewport = {
 
 export default async function IndexRoute({children}: {children: React.ReactNode}) {
   return (
-    <>
-      <CustomCursor />
-      <IntroAnimation />
-      <ScrollToTopButton />
-      <div className="flex min-h-screen flex-col bg-white text-black" style={{ paddingTop: 'var(--navbar-height, 96px)' }}>
-        <Navbar />
-        <div>{children}</div>
-        <Footer />
-      </div>
-      <Toaster />
-      
-      {/* Sanity 스튜디오에 있는 데이터의 실시간 업데이트를 위한 라이브 구독 컴포넌트 */}
-      <SanityLive onError={handleError} /> 
-      <SpeedInsights />
-    </>
+    <OverlayScrollbarsWrapper>
+      <GSAPScroll>
+        <CustomCursor />
+        <IntroAnimation />
+        <ScrollToTopButton />
+        <div className="flex min-h-screen flex-col bg-white text-black" style={{ paddingTop: 'var(--navbar-height, 96px)' }}>
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+        </div>
+        <Toaster />
+        
+        {/* Sanity 스튜디오에 있는 데이터의 실시간 업데이트를 위한 라이브 구독 컴포넌트 */}
+        <SanityLive onError={handleError} /> 
+        <SpeedInsights />
+      </GSAPScroll>
+    </OverlayScrollbarsWrapper>
   )
 }
